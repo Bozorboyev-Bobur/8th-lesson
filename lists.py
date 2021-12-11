26bf23f7c64c327e2915370efa3dcb13be3536d7
Bobur Bazarbayev <bozorboyevbobur2004@gmail.com>
2021-12-11 15:34:56 +0500

    first commit

---
+++ lists.py
@@ -0,0 +1,58 @@
+# cities = ['Tashkent', 'Bukhara', 'Samarkand', 'Khorezm', 'Fergana', 'Karshi', 'Nukus', 'Termez', 'Namangan', 'Andijan', '2']
+# three_cities = cities[3:6]
+# print(three_cities)
+# print(three_cities)
+# reversed_cities = cities[::-1]
+# print('Fergana' in cities)
+# print('Chirchik' in cities)
+# print('2' in cities)
+
+# for city in cities:
+#     print(city)
+
+# text = 'My name is Bobur. I 17 years old'
+# for letter in text:
+#     print(letter)
+
+# cities = ('Tashkent', 'Bukhara', 'Samarkand', 'Khorezm', 'Fergana', 'Karshi', 'Nukus', 'Termez', 'Namangan', 'Andijan', '2')
+
+# for city in cities:
+#     print(city) 
+
+# print(range(10))
+# # (0, 1 ,2 ,3, 4, 5, 6, 7, 8, 9)
+
+# for num in range(20, 10, -1):
+#     print(num)
+
+# Iterables
+# 1. lists - [] 
+# 2. tuples -()
+# 3. strings - '',""
+# 4. range()
+#
+# ЦИКЛ FOR нужен для того, чтобы прогнять через цикл ИНТЕРИРУЕМЫЕ СУЩНОСТИ
+
+# for i in range(0,1000,5):
+# #     print(i * i, i * i * i)
+
+# acc = 1
+# for num in range(1,11):
+#     acc = acc  * num
+# print(acc)
+
+
+# acc = 0
+# for num in range(0,21,2):
+#     acc = acc + num
+# print(acc)
+
+# cities = ['Tashkent', 'Bukhara', 'Samarkand', 'Khorezm', 'Fergana', 'Karshi', 'Nukus', 'Termez', 'Namangan', 'Andijan',]
+
+# acc = ''
+# cities_len = len(cities)
+# for i in range(cities_len):
+#     if i != cities_len - 1:
+#         acc += cities[i] + ', '
+#     else: acc += cities[i]
+# print(acc)
